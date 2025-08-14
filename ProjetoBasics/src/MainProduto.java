bec4e3da4f2d6340dcb7ad0b57c333a25726159b
Guilherme Araujo Silva <guilherme.silva@PROWAY.TREINA>
2025-08-13 22:01:01 -0300

    Adicionando CRUD para Produto

--- ProjetoBasics/src/MainProduto.java
+++ ProjetoBasics/src/MainProduto.java
@@ -1,21 +1,158 @@
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainProduto {
     public static void main(String[] args) {
 
-        // Instanciar objeto da classe Produto
-        Produto produto1 = new Produto();
+        // Aplicando o conceito de CRUD em List
+        // C - Create
+        // R - Read
+        // U - Update
+        // D - Delete
 
-        produto1.setNome(JOptionPane.showInputDialog("Digite o nome do produto"));
+        // Criando a lista do tipo da classe 'Produto'
+        List<Produto> produtoList = new ArrayList<>();
 
-        double valor = Double.parseDouble(JOptionPane.showInputDialog("Qual o valor do produto?"));
-        produto1.setValor(valor);
+        // Criar um contador para sequência dos códigos
+        int sequeciaCodigos = 1;
 
-        String mensagem = "Detalhes do produto\n" +
-                "\nProduto: " + produto1.getNome() +
-                "\nValor: RS" + produto1.getValor();
+        int opcao = 999;
+        String menu = "*** Menu Produto ***\n" +
+                "\n[1] Listar todos" +
+                "\n[2] Buscar por código" +
+                "\n[3] Adicionar novo" +
+                "\n[4] Alterar" +
+                "\n[5] Excluir" +
+                "\n[0] Sair";
 
-        JOptionPane.showMessageDialog(null, mensagem);
+        while (opcao != 0) {
+            opcao = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+            switch (opcao) {
+                case 1:
+                    String tabela = "<html><table border='1'>" +
+                            "<tr>" +
+                            "   <th>Código</th>" +
+                            "   <th>Nome</th>" +
+                            "   <th>Valor</th>" +
+                            "</tr>";
+
+                    for (Produto produto : produtoList) {
+                        tabela += "" +
+                                "<tr>" +
+                                "   <td>" + produto.getCodigo() + "</td>" +
+                                "   <td>" + produto.getNome() + "</td>" +
+                                "   <td>" + produto.getValor() + "</td>" +
+                                "</tr>";
+                    }
+
+                    tabela += "</table></html>";
+
+                    if (produtoList.isEmpty()) {
+                        JOptionPane.showMessageDialog(null, "Sem produtos cadastrados.");
+                    } else {
+                        JOptionPane.showMessageDialog(null, tabela);
+                    }
+                    break;
+
+                case 2:
+                    int codigoBusca = Integer.parseInt(JOptionPane.showInputDialog("Digite o código para buscar"));
+                    Produto produtoEncontrado = null;
+
+                    for (Produto produto : produtoList) {
+                        if (codigoBusca == produto.getCodigo()) {
+                            produtoEncontrado = produto;
+                        }
+                    }
+
+                    if (produtoEncontrado == null) {
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Produto não encontrado para o código " + codigoBusca);
+                    } else {
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Produto encontrado!\n" +
+                                        "\nCódigo: " + produtoEncontrado.getCodigo() +
+                                        "\nNome: " + produtoEncontrado.getNome() +
+                                        "\nValor: R$" + produtoEncontrado.getValor()
+                        );
+                    }
+
+                    break;
+
+                case 3:
+                    Produto novoProduto = new Produto();
+
+                    novoProduto.setCodigo(sequeciaCodigos);
+                    sequeciaCodigos++;
+
+                    novoProduto.setNome(JOptionPane.showInputDialog("Digite o nome do produto"));
+
+                    double novoValor = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do produto"));
+                    novoProduto.setValor(novoValor);
+
+                    produtoList.add(novoProduto);
+
+                    JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+                    break;
+
+                case 5:
+                    tabela = "<html><table border='1'>" +
+                            "<tr>" +
+                            "   <th>Código</th>" +
+                            "   <th>Nome</th>" +
+                            "   <th>Valor</th>" +
+                            "</tr>";
+
+                    for (Produto produto : produtoList) {
+                        tabela += "" +
+                                "<tr>" +
+                                "   <td>" + produto.getCodigo() + "</td>" +
+                                "   <td>" + produto.getNome() + "</td>" +
+                                "   <td>" + produto.getValor() + "</td>" +
+                                "</tr>";
+                    }
+
+                    tabela += "</table></html>" +
+                            "\nDigite o código para excluir";
+                    int codigoExclusao = Integer.parseInt(JOptionPane.showInputDialog(tabela));
+
+                    Produto produtoExcluir = null;
+                    for (Produto produto : produtoList) {
+                        if (produto.getCodigo() == codigoExclusao) {
+                            produtoExcluir = produto;
+                        }
+                    }
+
+                    if (produtoExcluir == null) {
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Produto não enontrado para o código " + codigoExclusao
+                        );
+                    } else {
+                        produtoList.remove(produtoExcluir);
+                        JOptionPane.showMessageDialog(
+                                null,
+                                "Produto excluído com sucesso!" +
+                                        "\n" + produtoExcluir.getCodigo() +
+                                        " - " + produtoExcluir.getNome()
+                        );
+                    }
+
+                    break;
+
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Saindo do sistema....");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida!");
+            }
+
+        }
+
 
     }
 }
